94feb97961359818d1a188cc960b30c3538550c2
eslafroggy <claire.pierce@gmail.com>
2013-06-14 11:45:49 -0700

    tidied up code a bit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  
+    
  before_filter :setup_twilio_client
 
- def setup_twilio_client
-  if @twilio_client.nil?
-    @twilio_client = Twilio::REST::Client.new("secret","secret") 
-  end
-end  
-
-def current_user
-  if session[:user_id]
-    @current_user = User.find(session[:user_id])
-  else
-    @current_user = nil
+  def setup_twilio_client
+    if @twilio_client.nil?
+      @twilio_client = Twilio::REST::Client.new("secret","secret") 
     end
-  end
-   helper_method :current_user
-end  
+  end  
+
+  def current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+   end
+     helper_method :current_user
+  end  
